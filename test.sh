5329f7c4f92566d0ac95f11f21e72ff66171f780
esarbia <fortpetc@yahoo.com>
2016-04-09 17:50:35 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export JAVA_HOME=/usr/local/jdk1.8.0_77
-/usr/local/jdk1.8.0_77/bin/javac HelloWorld.java
-chown tomcat:tomcat /usr/share/tomcat6/.jenkins/workspace/HelloWorld-TutPoint/*.class
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.77-0.b03.el6_7.x86_64
+/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.77-0.b03.el6_7.x86_64/bin/javac HelloWorld.java
+#chown tomcat:tomcat /usr/share/tomcat6/.jenkins/workspace/HelloWorld-TutPoint/*.class
 java HelloWorld
